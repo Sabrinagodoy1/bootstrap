f54abcd1769d9527b1572e17ce7f9ca443028423
Sabrina Godoy <sabrinapenteadogodoy2003@gmail.com>
2022-05-25 13:10:53 -0300

    atualização do bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
-/*Carousel
-*/
+let email = document.querySelector('#email')
+let emailOk = false
+let assuntoOk = false
+function validaEmail() {
+   let txtEmail = document.querySelector('#txtEmail') 
+
+   if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 ) {
+      txtEmail.innerHTML = 'E-mail inválido &#10060;'
+      txtEmail.style.color = 'red'
+   } else {
+      txtEmail.innerHTML = 'E-mail válido &#9989;'
+      txtEmail.style.color = 'green'
+      emailOk = true
+   }
+}
+function enviar() {
+    if ( emailOk == true) {
+       alert  ('completisso meu cumpade, Seja feliz!')
+    } else {
+       alert ('Preencha o formulário corretamente antes de enviar...')
+    }
+ }
+ 
+
 $('#carousel-example').on('slide.bs.carousel', function (e) {
     /*
         CC 2.0 License Iatek LLC 2018 - Attribution required
